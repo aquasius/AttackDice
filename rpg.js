901813aaf4a5929acda23b00549eca37b96b3ea0
Aaron Quasius <aquasius92@gmail.com>
2019-10-14 15:26:45 -0500

    created function for your characters power, boss health, and boss power. Am also in middle of creating function and prompts for Normal and heavy attacks

--- rpg.js
+++ rpg.js
@@ -1,7 +1,7 @@
 let chooseName = prompt("Please choose your character name","Name...");
     console.log(chooseName);
 
-    alert("Now you will decide your character attributes");
+    alert("The dice will now decide your character attributes");
 
      let fiftySidedDice = Math.floor(Math.random() * 50);
      function rollDie(sides){
@@ -18,3 +18,30 @@ function rollDice(sides){
     with(Math) return 1 + floor(random() * sides);
 }
 console.log("Your armor is set to",(fourSidedDice));
+
+let tenSidedDice = Math.floor(Math.random() * 10);
+function rollForPower(sides){
+    if(!sides) sides = 10
+    with(Math) return 1 + floor(random() * sides);
+}
+console.log("Your power is set to",(tenSidedDice));
+
+let bossHealth = 100;
+console.log("Dice Game, the Doom-bringers' health is set to",(bossHealth));
+
+let bossPower = 10;
+console.log("Dice Game, the Doom-Bringers' power is set to",(bossPower));
+
+alert("Battle begins\nYou will now enter your attack phase!");
+
+let rollSixSidedDice = Math.floor(Math.random() * 6);
+let normalAttack = 
+function rollSixSidedDice(side){
+prompt("Choose attack type")
+    if("" )
+    if(!sides) side = 6
+with(Math) return 1 + floor(random() * sides);
+
+}
+
+}
