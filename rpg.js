598e5d02bcabb476b0299a17b04d4a1b4a0562c1
Aaron Quasius <aquasius92@gmail.com>
2019-10-14 15:01:30 -0500

    got rid of unnecessary alerts that popped up prior to the console log and gamve sides a definition in my 2nd function

--- rpg.js
+++ rpg.js
@@ -8,14 +8,13 @@ let chooseName = prompt("Please choose your character name","Name...");
         if(!sides) sides = 50
         with(Math) return 1 + floor(random() * sides);
      }
-     alert("Your health is now",(fiftySidedDice));
      console.log("Your health is now",(fiftySidedDice));
     
+    
 
-let fourSidedDice = Math.floor(Math.random() * sides);
+let fourSidedDice = Math.floor(Math.random() * 4);
 function rollDice(sides){
     if(!sides) sides = 4
     with(Math) return 1 + floor(random() * sides);
 }
-alert("Your armor is currently",(fourSidedDice));
 console.log("Your armor is set to",(fourSidedDice));
