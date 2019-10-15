a64319c1a6928b4882ccecd01d1b3046d0484d6e
Aaron Quasius <aquasius92@gmail.com>
2019-10-15 08:12:05 -0500

    created my functions for normal and heavy attack. Also created 2 buttons for normal and heavy attacks.

--- rpg.js
+++ rpg.js
@@ -1,30 +1,26 @@
+
+
+function rollDice(sides){
+   return Math.ceil(Math.random() * sides)
+}
+
 let chooseName = prompt("Please choose your character name","Name...");
     console.log(chooseName);
 
     alert("The dice will now decide your character attributes");
 
-     let fiftySidedDice = Math.floor(Math.random() * 50);
-     function rollDie(sides){
-        if(!sides) sides = 50
-        with(Math) return 1 + floor(random() * sides);
-     }
+     let fiftySidedDice = rollDice(50);
+    
      console.log("Your health is now",(fiftySidedDice));
     
     
 
-let fourSidedDice = Math.floor(Math.random() * 4);
-function rollDice(sides){
-    if(!sides) sides = 4
-    with(Math) return 1 + floor(random() * sides);
-}
+let fourSidedDice = rollDice(4);
+
 console.log("Your armor is set to",(fourSidedDice));
 
-let tenSidedDice = Math.floor(Math.random() * 10);
-function rollForPower(sides){
-    if(!sides) sides = 10
-    with(Math) return 1 + floor(random() * sides);
-}
-console.log("Your power is set to",(tenSidedDice));
+let playerPower = rollDice(10);
+console.log("Your power is set to",(playerPower));
 
 let bossHealth = 100;
 console.log("Dice Game, the Doom-Bringers' health is set to",(bossHealth));
@@ -34,26 +30,46 @@ console.log("Dice Game, the Doom-Bringers' power is set to",(bossPower));
 
 alert("Battle begins\nYou will now enter your attack phase!");
 
-let rollSixSidedDice = Math.round(Math.random() * 6);
-let isValid = false;
-let userInputOne = "Normal Attack";
-let userInputTwo = "Heavy Attack";
 
-while(!isValid){
-    userInput = prompt("Please enter Normal or Heavy attack")
-    if(userInput === userInputOne) userInput = rollSixSidedDice;
+
+
+
+function heavyAttack(){
+    let heavyHitChance = rollDice(6);
+    if(heavyHitChance>4){
+        let heavyDamage = rollDice(6) + playerPower;
+        bossHealth -= heavyDamage;
     }
-    console.log(rollSixSidedDice);
+}
+
+
+function normalAttack(){
+    let normalHitChance = rollDice(6);
+    if(normalHitChance<=4){
+        let normalDamage = playerPower
+        bossHealth -= normalDamage;
+    }
+}
+// let rollSixSidedDice = Math.round(Math.random() * 6);
+// let isValid = false;
+// let userInputOne = "Normal Attack";
+// let userInputTwo = "Heavy Attack";
+
+// while(!isValid){
+//     userInput = prompt("Please enter Normal or Heavy attack")
+//     if(userInput === userInputOne) userInput = rollSixSidedDice;
+//     }
+//     console.log(rollSixSidedDice);
     
-//    if(userInput === userInputTwo){
+// //    if(userInput === userInputTwo){
 
-//    }
-// //  let normalAttack = rollSixSidedDice + rollForPower
-//  function rollForNormalAttackDamage(side){
-//      if(!sides) sides = 6
-//     with(Math) return 1 + round(random() * sides);
+// //    }
+// // //  let normalAttack = rollSixSidedDice + rollForPower
+// //  function rollForNormalAttackDamage(side){
+// //      if(!sides) sides = 6
+// //     with(Math) return 1 + round(random() * sides);
 
-//  }
+// //  }
 
-//  }
-//   console.log(rollSixSidedDice);
+// //  }
+// //   console.log(rollSixSidedDice);
