47336ac64814b94fa2aa2ba14e0ebc906ab62bc1
Aaron Quasius <aquasius92@gmail.com>
2019-10-16 16:21:16 -0500

    created a space for what I had my text console logged to, now updates in real time on the screen. Put my images into flex containers so I could even the spacing between the images and the VS text. That helped clean the overall layout up a bit. Removed the ugly black border around the health. Looks alot better without it. Tried to make it fully front end midway through my code. Ended up having it 95% front end functional. Which leaves m 95% satisfied

--- rpg.js
+++ rpg.js
@@ -31,7 +31,6 @@ function setStartGameVariables(){
     // Generated stats for character
 
      armorScore = rollDice(4);
-
     console.log("Your armor is set to",(armorScore));
 
      playerPower = rollDice(10);
@@ -43,6 +42,7 @@ console.log("Dice Game, the Doom-Bringers' health is set to",(bossHealth));
      bossPower = 10;
 console.log("Dice Game, the Doom-Bringers' power is set to",(bossPower));
 
+alert("Make sure to check the console log for your current stats!");
 alert("Battle begins\nYou will now enter your attack phase!");
 }
 
@@ -57,8 +57,7 @@ function normalAttack(){
     if(normalHitChance===4){
         let normalDamage = playerPower;
         bossHealth -= normalDamage;
-    console.log("You choose to play it safe. You step in for a swift slash! Doom Bringer says Bring it peasant! His health is now");
-    console.log(bossHealth);
+        document.getElementById("battleText").innerHTML ="You choose to play it safe. You step in for a swift slash! Doom Bringer says Bring it peasant!";
     }
    else if(normalHitChance===3){
     var popup = document.getElementById("myPopup");
@@ -66,8 +65,8 @@ function normalAttack(){
     popup.classList.toggle("show");    
     let normalDamage = playerPower
         bossHealth -= normalDamage;
-        console.log("You lunge for a quick chop! Doom Bringers' arm is badly cut! His health is now");
-        console.log(bossHealth);
+        document.getElementById("battleText").innerHTML ="You lunge for a quick chop! Doom Bringers' arm is badly cut!";
+        
     }
     
     else if(normalHitChance===2){
@@ -76,54 +75,75 @@ function normalAttack(){
         popup.classList.toggle("show"); 
         let normalDamage = playerPower; // changed
         bossHealth -= normalDamage;
-        console.log("Ayyyyyyahhh! You perform a leaping slash attack that just graces Doom Bringers' chest! YOU FOOL yells Doom Bringer. His health is now");
-        console.log(bossHealth);
+        document.getElementById("battleText").innerHTML = "You perform a leaping slash attack that just graces Doom Bringers' chest!";
     }
 
 
     else if(normalHitChance===1){
+        var popup = document.getElementById("myPopup");
+        popup.innerHTML = "AHHHH! Why won't you DIE!?";
+        popup.classList.toggle("show"); 
         let normalDamage = playerPower
         bossHealth -= normalDamage;
-        console.log("Shhhhckkk! You attempt a normal slash attack, but Doom Bringer brushes it off for minimal damage. His health is now");
-        console.log(bossHealth);
+        document.getElementById("battleText").innerHTML ="Shhhhckkk! You attempt a normal slash attack, but Doom Bringer brushes it off for minimal damage.";
     }
 
     if(bossHealth<=0){
+        var popup = document.getElementById("myPopup");
+        popup.innerHTML = "I AM THE NEW KING!!";
+        popup.classList.toggle("show"); 
         winCondition();
     }
 
     else if(normalHitChance>4){
-        console.log("Your attack missed! Watch out!");
+        var popup = document.getElementById("myPopup");
+        popup.innerHTML = "WHAAAA? How?!";
+        popup.classList.toggle("show"); 
+        document.getElementById("battleText").innerHTML = "Your attack missed! Watch out!";
         bossSwing();
         }
     document.getElementById("currentBoss").innerHTML = bossHealth;
     }
 //  created scenarios for each instance of possible dice that could be rolled and assign different dialogue for them
 function heavyAttack(){
+    var popup = document.getElementById("myPopup");
+        popup.innerHTML = "AYYYYYAHHHH!!!";
+        popup.classList.toggle("show"); 
     let heavyHitChance = rollDice(7);
     if(heavyHitChance===5){
         let heavyDamage = rollDice(7) + playerPower;
         bossHealth -= heavyDamage;
-        console.log("With all your might you grasp your sword with two hands and execute a lacerating slash! Doom Bringers' health is now");
-        console.log(bossHealth);
+        document.getElementById("battleText").innerHTML = "With all your might you grasp your sword with two hands and execute a lacerating slash!";
+        
     }
     if(heavyHitChance===6){
+        var popup = document.getElementById("myPopup");
+        popup.innerHTML = "HAHA! You aren't as smart as you are UGLY!";
+        popup.classList.toggle("show"); 
         let heavyDamage = rollDice(6) + playerPower;
         bossHealth -= heavyDamage;
-        console.log("You feint an attack! Doom Bringer falls for the feint and exposes an opening! You execute a perfectly placed blow! Doom Bringers' health is now");
-        console.log(bossHealth);
+        document.getElementById("battleText").innerHTML = "You feint an attack! Doom Bringer falls for the feint and exposes an opening! You execute a perfectly placed blow!";
+        
     }  
     
     if(bossHealth<=0){
+        var popup = document.getElementById("myPopup");
+        popup.innerHTML = "Knowledge ALWAYS outweighs physicality. AHHHH!";
+        popup.classList.toggle("show"); 
         winCondition();
 }
 
     else if(heavyHitChance<4){
-        console.log("You swing a brutal attack, but Dice Game - The Doombringer evades and gets a chance to attack!")
+        var popup = document.getElementById("myPopup");
+        popup.innerHTML = "What?! but.. but HOW?!";
+        popup.classList.toggle("show"); 
+        document.getElementById("battleText").innerHTML ="You swing a brutal attack, but Doombringer evades and gets a chance to attack!"
      bossSwing();   
     }
     document.getElementById("currentBoss").innerHTML = bossHealth;
 }
+    
+
 
 // boss attack stipulations 
 function bossSwing(){
@@ -131,22 +151,31 @@ function bossSwing(){
     popup.innerHTML = "YOU FOOL!";
     popup.classList.toggle("show");
     let bossAttack = rollDice(6);
-    if(bossAttack===5){
-        console.log("Dice Game hurls his axe and exerts an exhausting breath! You roll out of the way and escape damage!")
+if(bossAttack===5){
+    document.getElementById("battleText").innerHTML ="Dice Game hurls his axe and exerts an exhausting breath! You roll out of the way and escape damage!"; 
+    
     }
     if(bossAttack===6){
         var popup = document.getElementById("myPopup");
-    popup.innerHTML = "WHAT?! HOW?!";
-    popup.classList.toggle("show");
-        console.log("Dice Game, the Doom Bringer unleashes a monstrous slash! You slide underneath the axe in perfect range to execute your next attack!");
-    }
+        popup.innerHTML = "WHAT?! HOW?!";
+        popup.classList.toggle("show");
+        document.getElementById("battleText").innerHTML ="Dice Game, the Doom Bringer unleashes a monstrous slash! You slide underneath the axe in perfect range to execute your next attack!";
+        }
+    
         else if(bossAttack<5){
-        let bossAttackDamage = bossPower; //changed
-        playerHealth -= bossAttackDamage - armorScore;
-            console.log("Doom Bringer attacks and lands a crushing blow! Your health is now");
-        console.log(playerHealth);
+            var popup = document.getElementById("myPopup");
+            popup.innerHTML = "You puny peasant!";
+            popup.classList.toggle("show"); 
+            let bossAttackDamage = bossPower; //changed
+            playerHealth -= bossAttackDamage - armorScore;
+            document.getElementById("battleText").innerHTML ="Doom Bringer attacks and lands a crushing blow!";
+            
+    
         if(playerHealth<=0){
-            console.log("Argggghhhh! Doom Bringer lands a devastating blow!")
+            var popup = document.getElementById("myPopup");
+            popup.innerHTML = "Barely broke a sweat!";
+            popup.classList.toggle("show"); 
+            // console.log("Argggghhhh! Doom Bringer lands a devastating blow!")
             loseCondition();
             
         }
@@ -175,6 +204,7 @@ function printBossHealth(){
 function loseCondition(){
     let endPrompt;
     if(playerHealth<=0){
+        document.getElementById("battleText").innerHTML ="Argggghhhh! Doom Bringer lands a devastating blow!";
         endPrompt = prompt("You have failed to defeat the powerful Doom Bringer. Would you like to try again?");
     }
     if(endPrompt == "yes"){
